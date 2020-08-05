18d1f24f57df9c279f6edc1c91b10b2d1ffb0b76
ngocbq3 <bqngoc@gmail.com>
2020-08-05 14:36:39 +0700

    update layout bai4

---
+++ bai4/index.php
@@ -0,0 +1,109 @@
+<!--Phần đầu của website-->
+<?php include_once "layout/header.php" ?>
+
+<!--Phần nội dung của website-->
+<main>
+    <!--List products-->
+    <article>
+        <div class="col">
+            <div class="product">
+                <a href="#">
+                    <img src="images/xKINH-TE.jpg">
+                    <h3>Lorem ipsum dolor.</h3>
+                    <div class="price">13.000.000</div>
+                    <div class="desc">
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+                    </div>
+                </a>
+            </div>
+        </div>
+        <div class="col">
+            <div class="product">
+                <a href="#">
+                    <img src="images/xKINH-TE.jpg">
+                    <h3>Lorem ipsum dolor.</h3>
+                    <div class="price">13.000.000</div>
+                    <div class="desc">
+                        <p>Lorem ipsum dolor sit amet, </p>
+                    </div>
+                </a>
+            </div>
+        </div>
+        <div class="col">
+            <div class="product">
+                <a href="#">
+                    <img src="images/xKINH-TE.jpg">
+                    <h3>Lorem ipsum dolor.</h3>
+                    <div class="price">13.000.000</div>
+                    <div class="desc">
+                        <p>Lorem ipsum dolor sit amet, consectetur </p>
+                    </div>
+                </a>
+            </div>
+        </div>
+        <div class="col">
+            <div class="product">
+                <a href="#">
+                    <img src="images/xKINH-TE.jpg">
+                    <h3>Lorem ipsum dolor.</h3>
+                    <div class="price">13.000.000</div>
+                    <div class="desc">
+                        <p>Lorem ipsum dolor sit amet</p>
+                    </div>
+                </a>
+            </div>
+        </div>
+        <div class="col">
+            <div class="product">
+                <a href="#">
+                    <img src="images/xKINH-TE.jpg">
+                    <h3>Lorem ipsum dolor.</h3>
+                    <div class="price">13.000.000</div>
+                    <div class="desc">
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+                    </div>
+                </a>
+            </div>
+        </div>
+        <div class="col">
+            <div class="product">
+                <a href="#">
+                    <img src="images/xKINH-TE.jpg">
+                    <h3>Lorem ipsum dolor.</h3>
+                    <div class="price">13.000.000</div>
+                    <div class="desc">
+                        <p>Lorem ipsum dolor sit amet</p>
+                    </div>
+                </a>
+            </div>
+        </div>
+        <div class="col">
+            <div class="product">
+                <a href="#">
+                    <img src="images/xKINH-TE.jpg">
+                    <h3>Lorem ipsum dolor.</h3>
+                    <div class="price">19.000.000</div>
+                    <div class="desc">
+                        <p>Lorem ipsum dolor sit amet</p>
+                    </div>
+                </a>
+            </div>
+        </div>
+        <div class="col">
+            <div class="product">
+                <a href="#">
+                    <img src="images/xKINH-TE.jpg">
+                    <h3>Lorem ipsum dolor.</h3>
+                    <div class="price">13.000.000</div>
+                    <div class="desc">
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+                    </div>
+                </a>
+            </div>
+        </div>
+    </article>
+    <!--End list products-->
+</main>
+<!--Kết thúc phần nội dung-->
+
+<?php include_once "layout/footer.php" ?>
---
+++ bai4/layout/header.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Trang chủ</title>
+    <link rel="stylesheet" type="text/css" href="css/common.css">
+</head>
+
+<body>
+    <div class="container">
+        <header><img src="images/xbanner-trang-lien-he-moi.jpg.pagespeed.ic.FQvWHe7Pcx.jpg"></header>
+        <!--Menu-->
+        <nav>
+            <ul>
+                <li><a href="index.html">Trang chủ</a></li>
+                <li><a href="about.html">Giới thiệu</a></li>
+                <li><a href="news.html">Tin tức</a></li>
+                <li><a href="contact.html">Liên hệ</a></li>
+                <li><a href="album.html">Kho ảnh</a></li>
+            </ul>
+        </nav>
+        <!--End menu-->
